29cb7699e1884a925a412c480693f2a1f95f22ee
paulo <paulo.dasilva@terceiro-sky.com.br>
2023-01-13 17:16:24 -0300

    fix validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
-import { INestApplication, Logger } from '@nestjs/common';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as mongoose from 'mongoose';
-import mongoose_delete from 'mongoose-delete';
-import mongoosePaginate from 'mongoose-paginate-v2';
 import { AppModule } from './main/app.module';
 import { environment } from './main/config/environment';
 
@@ -24,6 +21,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api/bffmsregister/');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   setupSwagger(app);
   await app.listen(environment.port, () => {
     Logger.log('Listening at http://localhost:' + environment.port + '/');
