ceed9f72ba82a6346d72507478db808e0d959550
paulo <paulo.dasilva@terceiro-sky.com.br>
2023-04-15 22:38:52 -0300

    refactor: fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ import { environment } from './main/config/environment';
 
 const setupSwagger = (app: INestApplication) => {
   const options = new DocumentBuilder()
-    .setTitle('Finbot - Bff-ms-gateway-user')
+    .setTitle(`Finbot - ${environment.app.serviceName}`)
     .setDescription(
-      'Este microserviço registra usuários do tipo: cliente,profissional, admins e funcionários da empresa',
+      'Este microserviço registra usuários do tipo: cliente, parceiros homologados, admins e funcionários da empresa',
     )
     .setVersion('1.0.0')
     .addBearerAuth()
@@ -21,7 +21,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'https://frontend-finbot.herokuapp.com',
+      'https://finbot-frontend-hml.herokuapp.com/',
       'http://localhost:5173',
       '*',
       'localhost',
